7219a56860a53c2a87129c682819e08921aefe5c
mreyk <mercedesreyfandino@gmail.com>
2021-09-08 16:57:09 +0100

    Updated libraries in master

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CSV.Mixfile do
     [
       app: :csv,
       version: "2.4.1",
-      elixir: "~> 1.1",
+      elixir: "~> 1.12",
       deps: deps(),
       package: package(),
       docs: &docs/0,
@@ -41,15 +41,15 @@ defmodule CSV.Mixfile do
 
   defp deps do
     [
-      {:parallel_stream, "~> 1.0.4"},
-      {:excoveralls, "~> 0.13", only: :test},
-      {:benchfella, ">= 0.0.0", only: :bench},
-      {:ex_csv, ">= 0.0.0", only: :bench},
-      {:csvlixir, ">= 0.0.0", only: :bench},
-      {:cesso, ">= 0.0.0", only: :bench},
-      {:ex_doc, "~> 0.18", only: :docs},
-      {:inch_ex, "~> 0.5", only: :docs},
-      {:earmark, "~> 1.2", only: :docs}
+      {:parallel_stream, github: "mreyk/parallel_stream", branch: "fix_master"},
+      {:excoveralls, "~> 0.14.2", only: :test},
+      {:benchfella, "~> 0.3.5", only: :bench},
+      {:ex_csv, "~> 0.1.5", only: :bench},
+      {:csvlixir, "~> 2.0.4", only: :bench},
+      {:cesso, "~> 0.1.3", only: :bench},
+      {:ex_doc, "~> 0.25.2", only: :docs},
+      {:inch_ex, "~> 2.0.0", only: :docs},
+      {:earmark, "~> 1.4.15", only: :docs}
     ]
   end
 
